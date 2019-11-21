911699c2dd38f52695f249283b05af42fd6e0369
DrokeRavens <57990145+DrokeRavens@users.noreply.github.com>
2019-11-21 12:37:59 -0300

    v3
    • Alterações nos diretorios
    • Inicio de desenvolvimento do comando "mapa"
    • Alteração no sistema interno de ROOMS, vide Room.java. Agora as salas e suas direções são armazenadas via HashMap.

---
+++ src/Map.java
@@ -0,0 +1,17 @@
+/**
+ * @author  Grupo IFMG-Sabará
+ * @version v3
+ */
+public class Map
+{
+    private HashMap<String, Room> lastRooms;
+    public Map(HashMap<String, Room> lastRooms)
+    {
+        this.lastRooms = lastRooms;
+    }
+
+    public void drawMap(Room currentLocation)
+    {
+
+    }
+}
